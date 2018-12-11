30b16304c1d806ffb01b0c2b46ee96d077f51e58
Kate Drennen <k8ted11@gmail.com>
2018-12-11 16:46:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+var katzLine = []
+var count = 0
+// counter is set to 0 at the beginning of the day.
+// rewrite takeANumber to eliminating name
+// line is 3 people, but 2 are being served - line.slice[2]
+
+// line = [1,2,3] -- nowServing -> [2,3] line.length = 2 
+// takeANumber; position = 3
  
-function takeANumber(line, name) {
-  if (Array.isArray(line)) {
-    line.push(name);
-  }
-  var position = line.length;
-  return `Welcome, ${name}. You are number ${position} in line.`
+function takeANumber(line, ) {
+  var current = line.length; 
+  var next = position + 1;
+  count = 0;
+  return `Welcome, you are number ${next} in line.`
 }
 
 function nowServing(line) { 
