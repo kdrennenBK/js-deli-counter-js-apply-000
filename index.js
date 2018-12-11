2cd092821d9f1fa82e0f87103de57285c8d6e76b
Kate Drennen <k8ted11@gmail.com>
2018-12-11 17:14:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+var katzLine = []
+var count = 0
+// counter is set to 0 at the beginning of the day.
+// rewrite takeANumber eliminating name
+// line is 3 people, but 2 are being served - line.slice[2]
+
+// line = [1,2,3] -- nowServing -> [2,3] line.length = 2 
+// takeANumber; position = 3
  
+function trackingProgress(line, person) {
+  var count = 0;
+  var currentLine = line.length;
+  
+  return `Welcome, now serving number ${count}, you are next in line.`
+  
+}
 function takeANumber(line, name) {
-  if (Array.isArray(line)) {
-    line.push(name);
-  }
-  var position = line.length;
-  return `Welcome, ${name}. You are number ${position} in line.`
+  var position = line.length; 
+  return `Welcome ${name} , you are number ${position} in line.`
 }
 
 function nowServing(line) { 
@@ -12,6 +24,7 @@ function nowServing(line) {
     return "There is nobody waiting to be served!";
     } else {
       var currentCustomer = line.shift();
+      // var currentLine = line; // added 12.11
     }
   return `Currently serving ${currentCustomer}.`;
 }
